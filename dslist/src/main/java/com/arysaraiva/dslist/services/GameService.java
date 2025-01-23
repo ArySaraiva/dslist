98651109de5154fabc4e8fdccf51de8d9a4312de
Ary Saraiva <arysaraiva2311@gmail.com>
2025-01-23 10:56:48 -0400

    GET game by id

--- dslist/src/main/java/com/arysaraiva/dslist/services/GameService.java
+++ dslist/src/main/java/com/arysaraiva/dslist/services/GameService.java
@@ -1,14 +1,16 @@
 package com.arysaraiva.dslist.services;
 
+import com.arysaraiva.dslist.dto.GameDTO;
 import com.arysaraiva.dslist.dto.GameMinDTO;
 
 import com.arysaraiva.dslist.entities.Game;
 import com.arysaraiva.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Service
 public class GameService {
@@ -16,6 +18,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        Game result = gameRepository.findById(id).get();
+        GameDTO dto = new GameDTO(result);
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
        List<Game> result = gameRepository.findAll();
        return result.stream().map(x -> new GameMinDTO(x)).toList();
